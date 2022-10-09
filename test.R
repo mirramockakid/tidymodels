aabf94780543f4b61061fe55b60abee2deed2729
mirramockakid <mirramockakid@gmail.com>
2022-10-09 15:48:38 +0100

    setup

--- test.R
+++ test.R
@@ -1,6 +1,5 @@
 install.packages("modeldata")
 install.packages("tidymodels")
-library(ggplot2)
 library(tidymodels)
 
 
@@ -65,3 +64,35 @@ lm_fit <- parsnip::fit(lm_workflow, ames_train)
 
 # PREDICT
 predict(lm_fit, ames_testing)
+# === END === #
+
+
+# === F. RECIPE === #
+ames_recipe <-
+  recipes::recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + 
+                    Bldg_Type + Latitude + Longitude , data = ames_train) |>
+  recipes::step_log(Gr_Liv_Area, base = 10) |>
+  recipes::step_other(Neighborhood, threshold = 0.01) |>
+  recipes::step_dummy(recipes::all_nominal_predictors()) |>
+  recipes::step_interact(~ Gr_Liv_Area:starts_with("Bldg_Type_")) |>
+  recipes::step_ns(Latitude, Longitude, deg_free = 20)
+
+# look at recipe (tidy format)
+# id can be set in step function and helps describe the step0
+tidy(ames_recipe)
+
+lm_model <- parsnip::linear_reg() |>
+  parsnip::set_engine("lm")
+
+lm_workflow <- 
+  workflows::workflow() |>
+  workflows::add_model(lm_model) |>
+  workflows::add_recipe(ames_recipe)
+
+# FIT
+lm_fit <- parsnip::fit(lm_workflow, ames_train)
+
+# PREDICT
+predict(lm_fit, ames_testing)
+
+# === END === #
